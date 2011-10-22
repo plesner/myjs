e399ee3b66520dad3c6e807a916d41e3c4ed010e
Christian Plesner Hansen <plesner@ne.utrino.org>
2011-10-22 16:29:28 +1100

    Removed visitors from syntax trees.

--- src/ast.js
+++ src/ast.js
@@ -28,21 +28,6 @@ function defineAst(namespace, env) { // offset: 12
 
   namespace.internal = {};
 
-  function accept(parent, node, visitor, skipOpt) {
-    if (node && node.accept) {
-      return node.accept(visitor);
-    } else if (!skipOpt) {
-      throw parent;
-    }
-  }
-
-  function acceptAll(parent, nodes, visitor) {
-    var i;
-    for (i = 0; i < nodes.length; i++) {
-      accept([i, parent], nodes[i], visitor);
-    }
-  }
-
   namespace.internal.TextFormatter = TextFormatter;
   function TextFormatter(settings) {
     this.settings = settings;
@@ -117,32 +102,19 @@ function defineAst(namespace, env) { // offset: 12
     return this.tokens.join("");
   };
 
-  namespace.Visitor = Visitor;
-  function Visitor() { }
-
   namespace.Node = Node;
   function Node() { }
 
-  Node.prototype.traverse = function (visitor) {
-    throw new Error("Abstract method called on " + this.constructor);
-  };
+  inherits(Statement, Node);
+  namespace.Statement = Statement;
+  function Statement() { }
 
-  Node.prototype.accept = function (visitor) {
-    return this.traverse(visitor);
-  };
+  inherits(Expression, Node);
+  namespace.Expression = Expression;
+  function Expression() { }
 
   Node.prototype.unparse = function (out) {
     out.string("#<" + this.constructor.name + ">");
-  };
-
-  /*
-  Node.prototype.translate = function () {
-    return this;
-  };
-  */
-
-  Visitor.prototype.visitNode = function (node) {
-    return node.traverse(this);
   };
 
   function translateAll(elms) {
@@ -174,18 +146,6 @@ function defineAst(namespace, env) { // offset: 12
     });
   };
 
-  Program.prototype.accept = function (visitor) {
-    return visitor.visitProgram(this);
-  };
-
-  Program.prototype.traverse = function (visitor) {
-    acceptAll(this, this.elements, visitor);
-  };
-
-  Visitor.prototype.visitProgram = function (node) {
-    return this.visitNode(node);
-  };
-
   namespace.FunctionDeclaration = FunctionDeclaration;
   inherits(FunctionDeclaration, Node);
   function FunctionDeclaration(name, params, body) {
@@ -203,18 +163,6 @@ function defineAst(namespace, env) { // offset: 12
     out.string("function ").string(this.name).string("(")
       .strings(this.params, ", ").string(") {").indent().newline()
       .nodes(this.body).deindent().string("}").newline();
-  };
-
-  FunctionDeclaration.prototype.accept = function (visitor) {
-    visitor.visitFunctionDeclaration(this);
-  };
-
-  FunctionDeclaration.prototype.traverse = function (visitor) {
-    acceptAll(this, this.body, visitor);
-  };
-
-  Visitor.prototype.visitFunctionDeclaration = function (node) {
-    this.visitNode(node);
   };
 
   namespace.ReturnStatement = ReturnStatement;
@@ -239,20 +187,6 @@ function defineAst(namespace, env) { // offset: 12
     }
   };
 
-  ReturnStatement.prototype.accept = function (visitor) {
-    visitor.visitReturnStatement(this);
-  };
-
-  ReturnStatement.prototype.traverse = function (visitor) {
-    if (this.value) {
-      accept(this, this.value, visitor, true);
-    }
-  };
-
-  Visitor.prototype.visitReturnStatement = function (node) {
-    this.visitStatement(node);
-  };
-
   namespace.ThrowStatement = ThrowStatement;
   inherits(ThrowStatement, Statement);
   function ThrowStatement(value) {
@@ -261,18 +195,6 @@ function defineAst(namespace, env) { // offset: 12
 
   ThrowStatement.prototype.unparse = function (out) {
     out.string("throw ").node(this.value).string(";").newline();
-  };
-
-  ThrowStatement.prototype.accept = function (visitor) {
-    visitor.visitThrowStatement(this);
-  };
-
-  ThrowStatement.prototype.traverse = function (visitor) {
-    accept(this, this.value, visitor, true);
-  };
-
-  Visitor.prototype.visitThrowStatement = function (node) {
-    this.visitStatement(node);
   };
 
   namespace.Block = Block;
@@ -290,18 +212,6 @@ function defineAst(namespace, env) { // offset: 12
       .string("}");
   };
 
-  Block.prototype.accept = function (visitor) {
-    visitor.visitBlock(this);
-  };
-
-  Block.prototype.traverse = function (visitor) {
-    acceptAll(this, this.stmts, visitor);
-  };
-
-  Visitor.prototype.visitBlock = function (node) {
-    this.visitStatement(node);
-  };
-
   namespace.VariableStatement = VariableStatement;
   inherits(VariableStatement, Statement);
   function VariableStatement(decls) {
@@ -314,18 +224,6 @@ function defineAst(namespace, env) { // offset: 12
 
   VariableStatement.prototype.unparse = function (out) {
     out.string("var ").nodes(this.decls, ", ").string(";").newline();
-  };
-
-  VariableStatement.prototype.accept = function (visitor) {
-    visitor.visitVariableStatement(this);
-  };
-
-  VariableStatement.prototype.traverse = function (visitor) {
-    acceptAll(this, this.decls, visitor);
-  };
-
-  Visitor.prototype.visitVariableStatement = function (node) {
-    this.visitStatement(node);
   };
 
   namespace.VariableDeclaration = VariableDeclaration;
@@ -344,29 +242,6 @@ function defineAst(namespace, env) { // offset: 12
     if (this.value) {
       out.string(" = (").node(this.value).string(")");
     }
-  };
-
-  VariableDeclaration.prototype.accept = function (visitor) {
-    visitor.visitVariableDeclaration(this);
-  };
-
-  VariableDeclaration.prototype.traverse = function (visitor) {
-    if (this.value) {
-      accept(this, this.value, visitor, true);
-    }
-  };
-
-  Visitor.prototype.visitVariableDeclaration = function (node) {
-    this.visitNode(node);
-  };
-
-  inherits(Statement, Node);
-  function Statement() {
-
-  }
-
-  Visitor.prototype.visitStatement = function (node) {
-    this.visitNode(node);
   };
 
   namespace.IfStatement = IfStatement;
@@ -392,22 +267,6 @@ function defineAst(namespace, env) { // offset: 12
     out.newline();
   };
 
-  IfStatement.prototype.accept = function (visitor) {
-    visitor.visitIfStatement(this);
-  };
-
-  IfStatement.prototype.traverse = function (visitor) {
-    accept(this, this.cond, visitor, true);
-    accept(this, this.thenPart, visitor);
-    if (this.elsePart) {
-      accept(this, this.elsePart, visitor);
-    }
-  };
-
-  Visitor.prototype.visitIfStatement = function (node) {
-    this.visitStatement(node);
-  };
-
   namespace.SwitchStatement = SwitchStatement;
   inherits(SwitchStatement, Statement);
   function SwitchStatement(value, cases) {
@@ -419,19 +278,6 @@ function defineAst(namespace, env) { // offset: 12
     out.string("switch (").node(this.value).string(") {")
       .indent().newline().nodes(this.cases).deindent()
       .string("}").newline();
-  };
-
-  SwitchStatement.prototype.accept = function (visitor) {
-    visitor.visitSwitchStatement(this);
-  };
-
-  SwitchStatement.prototype.traverse = function (visitor) {
-    accept(this, this.value, visitor, true);
-    acceptAll(this, this.cases, visitor);
-  };
-
-  Visitor.prototype.visitSwitchStatement = function (node) {
-    this.visitStatement(node);
   };
 
   namespace.SwitchCase = SwitchCase;
@@ -448,21 +294,6 @@ function defineAst(namespace, env) { // offset: 12
       out.string("default: ");
     }
     out.indent().newline().nodes(this.body).deindent().newline();
-  };
-
-  SwitchCase.prototype.accept = function (visitor) {
-    visitor.visitSwitchCase(this);
-  };
-
-  SwitchCase.prototype.traverse = function (visitor) {
-    if (this.test) {
-      accept(this, this.test, visitor, true);
-    }
-    acceptAll(this, this.body, visitor);
-  };
-
-  Visitor.prototype.visitSwitchCase = function (node) {
-    this.visitNode(node);
   };
 
   namespace.DoStatement = DoStatement;
@@ -484,19 +315,6 @@ function defineAst(namespace, env) { // offset: 12
       .node(this.body).newline();
   };
 
-  WhileStatement.prototype.accept = function (visitor) {
-    visitor.visitWhileStatement(this);
-  };
-
-  WhileStatement.prototype.traverse = function (visitor) {
-    accept(this, this.cond, visitor, true);
-    accept(this, this.body, visitor);
-  };
-
-  Visitor.prototype.visitWhileStatement = function (node) {
-    this.visitStatement(node);
-  };
-
   namespace.ForStatement = ForStatement;
   inherits(ForStatement, Statement);
   function ForStatement(init, test, update, body) {
@@ -512,27 +330,6 @@ function defineAst(namespace, env) { // offset: 12
       .string(") ").node(this.body).newline();
   };
 
-  ForStatement.prototype.accept = function (visitor) {
-    visitor.visitForStatement(this);
-  };
-
-  ForStatement.prototype.traverse = function (visitor) {
-    if (this.init) {
-      accept(this, this.init, visitor, true);
-    }
-    if (this.test) {
-      accept(this, this.test, visitor, true);
-    }
-    if (this.update) {
-      accept(this, this.update, visitor, true);
-    }
-    accept(this, this.body, visitor);
-  };
-
-  Visitor.prototype.visitForStatement = function (node) {
-    this.visitStatement(node);
-  };
-
   namespace.ForInStatement = ForInStatement;
   inherits(ForInStatement, Statement);
   function ForInStatement(target, source, body) {
@@ -540,20 +337,6 @@ function defineAst(namespace, env) { // offset: 12
     this.source = source;
     this.body = body;
   }
-
-  ForInStatement.prototype.accept = function (visitor) {
-    visitor.visitForInStatement(this);
-  };
-
-  ForInStatement.prototype.traverse = function (visitor) {
-    accept(this, this.target, visitor, true);
-    accept(this, this.source, visitor, true);
-    accept(this, this.body, visitor);
-  };
-
-  Visitor.prototype.visitForInStatement = function (node) {
-    this.visitStatement(node);
-  };
 
   namespace.ContinueStatement = ContinueStatement;
   inherits(ContinueStatement, Statement);
@@ -575,27 +358,6 @@ function defineAst(namespace, env) { // offset: 12
     return new ExpressionStatement(this.expr.translate());
   };
 
-  ExpressionStatement.prototype.accept = function (visitor) {
-    return visitor.visitExpressionStatement(this);
-  };
-
-  ExpressionStatement.prototype.traverse = function (visitor) {
-    accept(this, this.expr, visitor, true);
-  };
-
-  Visitor.prototype.visitExpressionStatement = function (node) {
-    return this.visitStatement(node);
-  };
-
-  inherits(Expression, Node);
-  function Expression() {
-
-  }
-
-  Visitor.prototype.visitExpression = function (node) {
-    this.visitNode(node);
-  };
-
   namespace.AssignmentExpression = AssignmentExpression;
   inherits(AssignmentExpression, Expression);
   function AssignmentExpression(target, op, source) {
@@ -614,19 +376,6 @@ function defineAst(namespace, env) { // offset: 12
       .node(this.source).string(")");
   };
 
-  AssignmentExpression.prototype.accept = function (visitor) {
-    visitor.visitAssignmentExpression(this);
-  };
-
-  AssignmentExpression.prototype.traverse = function (visitor) {
-    accept(this, this.target, visitor, true);
-    accept(this, this.source, visitor, true);
-  };
-
-  Visitor.prototype.visitAssignmentExpression = function (node) {
-    this.visitExpression(node);
-  };
-
   namespace.InfixExpression = InfixExpression;
   inherits(InfixExpression, Expression);
   function InfixExpression(left, op, right) {
@@ -643,19 +392,6 @@ function defineAst(namespace, env) { // offset: 12
   InfixExpression.prototype.unparse = function (out) {
     out.string("(").node(this.left).string(") " + this.op + " (")
       .node(this.right).string(")");
-  };
-
-  InfixExpression.prototype.accept = function (visitor) {
-    return visitor.visitInfixExpression(this);
-  };
-
-  InfixExpression.prototype.traverse = function (visitor) {
-    accept(this, this.left, visitor, true);
-    accept(this, this.right, visitor, true);
-  };
-
-  Visitor.prototype.visitInfixExpression = function (node) {
-    return this.visitExpression(node);
   };
 
   namespace.ConditionalExpression = ConditionalExpression;
@@ -679,20 +415,6 @@ function defineAst(namespace, env) { // offset: 12
       .string(")");
   };
 
-  ConditionalExpression.prototype.accept = function (visitor) {
-    visitor.visitConditionalExpression(this);
-  };
-
-  ConditionalExpression.prototype.traverse = function (visitor) {
-    accept(this, this.cond, visitor, true);
-    accept(this, this.thenPart, visitor, true);
-    accept(this, this.elsePart, visitor, true);
-  };
-
-  Visitor.prototype.visitConditionalExpression = function (node) {
-    this.visitExpression(node);
-  };
-
   namespace.FunctionExpression = FunctionExpression;
   inherits(FunctionExpression, Expression);
   function FunctionExpression(name, params, body) {
@@ -710,18 +432,6 @@ function defineAst(namespace, env) { // offset: 12
     out.string("function ").string(this.name).string("(")
       .strings(this.params, ", ").string(") {").indent().newline()
       .nodes(this.body).deindent().string("}");
-  };
-
-  FunctionExpression.prototype.accept = function (visitor) {
-    visitor.visitFunctionExpression(this);
-  };
-
-  FunctionExpression.prototype.traverse = function (visitor) {
-    acceptAll(this, this.body, visitor);
-  };
-
-  Visitor.prototype.visitFunctionExpression = function (node) {
-    return this.visitExpression(node);
   };
 
   namespace.GetPropertyExpression = GetPropertyExpression;
@@ -773,19 +483,6 @@ function defineAst(namespace, env) { // offset: 12
   CallExpression.prototype.unparse = function (out) {
     out.string("(").node(this.base).string(")(")
       .nodes(this.args, ", ").string(")");
-  };
-
-  CallExpression.prototype.accept = function (visitor) {
-    return visitor.visitCallExpression(this);
-  };
-
-  CallExpression.prototype.traverse = function (visitor) {
-    accept(this, this.base, visitor, true);
-    accept(this, this.args, visitor, true);
-  };
-
-  Visitor.prototype.visitCallExpression = function (node) {
-    this.visitExpression(node);
   };
 
   namespace.UnaryExpression = UnaryExpression;
@@ -841,18 +538,6 @@ function defineAst(namespace, env) { // offset: 12
 
   Literal.prototype.unparse = function (out) {
     out.string(this.value);
-  };
-
-  Literal.prototype.accept = function (visitor) {
-    return visitor.visitLiteral(this);
-  };
-
-  Literal.prototype.traverse = function (visitor) {
-    // ignore
-  };
-
-  Visitor.prototype.visitLiteral = function (node) {
-    return this.visitExpression(node);
   };
 
   namespace.Identifier = Identifier;
